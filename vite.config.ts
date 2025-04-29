375fc03845aae3100a1f8f4dbf0e19d50ae28cb6
Paul <github@goonie.nl>
2025-04-29 13:44:48 +0200

    Configured path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, './src'),
+    },
+  },
   server: {
-    port: 8000
-  }
-})
+    port: 8000,
+  },
+});
