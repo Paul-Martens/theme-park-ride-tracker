4e7cfe33b569605ae9ac84883309e8eab38d7277
Paul <github@goonie.nl>
2025-04-30 14:32:10 +0200

    Added Supabase and set up local development

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Route, Routes } from 'react-router';
 
+import { useUser } from '~/services/supabase';
+
 import { routes } from '~/routes';
 
 import './App.css';
 
 function App() {
+  const { isPending } = useUser();
+
+  if (isPending) return null;
+
   return (
     <Routes>
       {routes.map((route) => (
---
+++ src/services/supabase.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  import.meta.env.VITE__SUPABASE__URL,
+  import.meta.env.VITE__SUPABASE__KEY,
+);
+
+function useUser() {
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(() => {
+      setIsPending(false);
+    });
+  }, []);
+
+  return { isPending };
+}
+
+export { useUser };
