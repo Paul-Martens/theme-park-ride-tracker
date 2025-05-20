7982fe58805384b84010d0499cacd45bc23c7d25
Paul <github@goonie.nl>
2025-05-20 16:04:56 +0200

    Added useSession hook to fetch current user session

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,8 +1,37 @@
+import { useEffect, useState } from 'react';
+
 import { createClient } from '@supabase/supabase-js';
+
+import type { Session } from '@supabase/supabase-js';
 
 const supabase = createClient(
   import.meta.env.VITE__SUPABASE__URL,
   import.meta.env.VITE__SUPABASE__KEY,
 );
 
-export { supabase };
+function useSession() {
+  const [session, setSession] = useState<Session | null>();
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+      setIsPending(false);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_, session) => {
+      setSession(session);
+      setIsPending(false);
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
+
+  return { session, isPending };
+}
+
+export { supabase, useSession };
