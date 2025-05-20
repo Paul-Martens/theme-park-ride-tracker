b3b45d58f5f09ffab08fc35ff4bfb781b957f3da
Paul <github@goonie.nl>
2025-05-20 17:26:29 +0200

    Added Page ui component and first styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
+import { motion } from 'motion/react';
+
+import { useSession } from '~/services/supabase';
+
+import { Page } from '~/ui/layout/Page';
+
+import './App.css';
+
 function App() {
+  const { isPending } = useSession();
+
+  if (isPending) {
+    return null;
+  }
+
   return (
-    <main>
-      <h1>Theme Park Ride Tracker</h1>
-    </main>
+    <Page>
+      <motion.h1
+        initial={{
+          opacity: 0,
+          y: 20,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          delay: 0.25,
+          duration: 0.25,
+        }}
+      >
+        Theme Park Ride Tracker
+      </motion.h1>
+    </Page>
   );
 }
 
---
+++ src/ui/layout/Page.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from 'react';
+
+import './Page.css';
+
+interface PageProps {
+  children?: ReactNode;
+}
+
+function Page({ children }: PageProps) {
+  return <main className="Page">{children}</main>;
+}
+
+export { Page };
