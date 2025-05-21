d427750f386bcf1eb6f56988945ad5d252793d00
Paul <github@goonie.nl>
2025-05-21 09:57:10 +0200

    Configured Playwright baseURL

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test.describe('App', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:8010');
+    await page.goto('/');
   });
 
   test('Shows app name as title', async ({ page }) => {
