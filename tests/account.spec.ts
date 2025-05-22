97ecca4dfb8f07d7f33eeefd5c87a041a107e59c
Paul <github@goonie.nl>
2025-05-22 14:35:47 +0200

    Added Supabase account helpers for tests and fixed flaky tests

--- tests/account.spec.ts
+++ tests/account.spec.ts
@@ -1,83 +1,137 @@
-import { expect, test } from '@playwright/test';
+import { expect, test as base } from '@playwright/test';
 
-import { deleteAllUsers } from './helpers/supabase';
+import {
+  createTemporaryUser,
+  generateTemporaryCredentials,
+} from './helpers/supabase';
 
-test.describe.serial('Account', () => {
-  test.beforeAll(async () => {
-    deleteAllUsers();
+import { deleteTemporaryUserByEmail } from './helpers/supabase';
+
+import type { Credentials } from './helpers/supabase';
+
+/**
+ * Generate temporary credentials and add them as a fixture.
+ */
+const test = base.extend<{ credentials: Credentials }>({
+  credentials: ({}, use) => {
+    const credentials = generateTemporaryCredentials();
+    use(credentials);
+  },
+});
+
+test.describe('Account', () => {
+  test.afterEach(async ({ credentials }) => {
+    await deleteTemporaryUserByEmail(credentials.email);
   });
 
-  test.afterAll(async () => {
-    deleteAllUsers();
+  /**
+   * The Sign Up form registers new users. During local development,
+   * users are automatically confirmed and active after signing up.
+   * On production, a confirmation email is sent first.
+   */
+  test.describe('Sign Up', () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto('/account/sign-up');
+    });
+
+    /**
+     * The Sign Up form should render its fields without issues.
+     */
+    test('Sign Up form renders without issues', async ({ page }) => {
+      await expect(page.getByLabel('Email Address')).toBeVisible();
+      await expect(page.getByLabel('Password')).toBeVisible();
+      await expect(page.getByRole('button', { name: 'Sign Up' })).toBeVisible();
+    });
+
+    /**
+     * When signing up a new account, a message about email
+     * verification should show.
+     */
+    test('Show a message after signing up', async ({ page, credentials }) => {
+      const message =
+        'Thank you for signing up. Please check your email to activate your account.';
+
+      await expect(page.getByText(message)).not.toBeVisible();
+
+      await page.getByLabel('Email Address').fill(credentials.email);
+      await page.getByLabel('Password').fill(credentials.password);
+      await page.getByRole('button', { name: 'Sign Up' }).click();
+
+      await expect(page.getByText(message)).toBeVisible();
+    });
+
+    /**
+     * Signing up an existing account should show an error.
+     */
+    test('Show an error message when trying to sign up an existing account', async ({
+      page,
+      credentials,
+    }) => {
+      const message = 'User already registered';
+
+      await createTemporaryUser(credentials);
+
+      await expect(page.getByText(message)).not.toBeVisible();
+
+      await page.getByLabel('Email Address').fill(credentials.email);
+      await page.getByLabel('Password').fill(credentials.password);
+      await page.getByRole('button', { name: 'Sign Up' }).click();
+
+      await expect(page.getByText(message)).toBeVisible();
+    });
   });
 
-  test('Sign Up form renders without issues', async ({ page }) => {
-    await page.goto('/account/sign-up');
+  /**
+   * The Sign In form is used to log into user accounts.
+   */
+  test.describe('Sign In', () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto('/account/sign-in');
+    });
 
-    await expect(page.getByLabel('Email Address')).toBeVisible();
-    await expect(page.getByLabel('Password')).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Sign Up' })).toBeVisible();
-  });
+    /**
+     * The Sign In form should render its fields without issues.
+     */
+    test('Sign In form renders without issues', async ({ page }) => {
+      await expect(page.getByLabel('Email Address')).toBeVisible();
+      await expect(page.getByLabel('Password')).toBeVisible();
+      await expect(page.getByRole('button', { name: 'Sign In' })).toBeVisible();
+    });
 
-  test('Sign In form renders without issues', async ({ page }) => {
-    await page.goto('/account/sign-in');
+    /**
+     * After signing in, the user should be redirect to Home.
+     */
+    test('Redirect to home after signing in', async ({ page, credentials }) => {
+      await createTemporaryUser(credentials);
 
-    await expect(page.getByLabel('Email Address')).toBeVisible();
-    await expect(page.getByLabel('Password')).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Sign In' })).toBeVisible();
-  });
+      await page.getByLabel('Email Address').fill(credentials.email);
+      await page.getByLabel('Password').fill(credentials.password);
+      await page.getByRole('button', { name: 'Sign In' }).click();
 
-  test('Show an error message for signing in with invalid credentials', async ({
-    page,
-  }) => {
-    await page.goto('/account/sign-in');
+      await expect(page).toHaveURL('/');
 
-    await expect(page.getByText('Invalid login credentials')).not.toBeVisible();
+      await expect(
+        page.getByRole('heading', { name: 'Theme Park Ride Tracker' }),
+      ).toBeVisible();
+    });
 
-    // These are the credentials we'll use when signing up.
-    // At this point, the account does not exist.
-    await page.getByLabel('Email Address').fill('test@goonie.nl');
-    await page.getByLabel('Password').fill('test1234');
-    await page.getByRole('button', { name: 'Sign In' }).click();
+    /**
+     * Signing into a non-existant account or with the wrong
+     * credentials should show an error.
+     */
+    test('Show an error message for signing in with invalid credentials', async ({
+      page,
+      credentials,
+    }) => {
+      const message = 'Invalid login credentials';
 
-    await expect(page.getByText('Invalid login credentials')).toBeVisible();
-  });
+      await expect(page.getByText(message)).not.toBeVisible();
 
-  test('Show a message after signing up', async ({ page }) => {
-    await page.goto('/account/sign-up');
+      await page.getByLabel('Email Address').fill(credentials.email);
+      await page.getByLabel('Password').fill(credentials.password);
+      await page.getByRole('button', { name: 'Sign In' }).click();
 
-    await page.getByLabel('Email Address').fill('test@goonie.nl');
-    await page.getByLabel('Password').fill('test1234');
-    await page.getByRole('button', { name: 'Sign Up' }).click();
-
-    await expect(
-      page.getByText(
-        'Thank you for signing up. Please check your email to activate your account.',
-      ),
-    ).toBeVisible();
-  });
-
-  test('Show an error message when trying to sign up an existing account', async ({
-    page,
-  }) => {
-    await page.goto('/account/sign-up');
-
-    await page.getByLabel('Email Address').fill('test@goonie.nl');
-    await page.getByLabel('Password').fill('test1234');
-    await page.getByRole('button', { name: 'Sign Up' }).click();
-
-    await expect(page.getByText('User already registered')).toBeVisible();
-  });
-
-  test('Redirect to home after signing in', async ({ page }) => {
-    await page.goto('/account/sign-in');
-
-    await page.getByLabel('Email Address').fill('test@goonie.nl');
-    await page.getByLabel('Password').fill('test1234');
-    await page.getByRole('button', { name: 'Sign In' }).click();
-
-    await expect(
-      page.getByRole('heading', { name: 'Theme Park Ride Tracker' }),
-    ).toBeVisible();
+      await expect(page.getByText(message)).toBeVisible();
+    });
   });
 });
--- tests/helpers/supabase.ts
+++ tests/helpers/supabase.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
+import { v4 } from 'uuid';
+
 import { createClient } from '@supabase/supabase-js';
 
 dotenv.config({ path: path.resolve('.env.local') });
@@ -10,7 +12,40 @@ const supabase = createClient(
   process.env.VITE__SUPABASE__SERVICE_KEY as string,
 );
 
-async function deleteAllUsers() {
+interface Credentials {
+  id: string;
+  email: string;
+  password: string;
+}
+
+function generateTemporaryCredentials(): Credentials {
+  const id = v4();
+  const email = `${id}@example.com`;
+  const password = 'test1234';
+
+  return { id, email, password };
+}
+
+async function createTemporaryUser(credentials: Credentials) {
+  const { error } = await supabase.auth.admin.createUser({
+    email_confirm: true,
+    ...credentials,
+  });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+}
+
+async function deleteTemporaryUserById(id: string) {
+  const { error } = await supabase.auth.admin.deleteUser(id);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+}
+
+async function deleteTemporaryUserByEmail(email: string) {
   const {
     data: { users },
     error,
@@ -20,9 +55,18 @@ async function deleteAllUsers() {
     throw new Error(error.message);
   }
 
-  users.forEach(async (user) => {
-    await supabase.auth.admin.deleteUser(user.id);
-  });
+  const user = users.find((user) => user.email === email);
+
+  if (user) {
+    deleteTemporaryUserById(user.id);
+  }
 }
 
-export { deleteAllUsers };
+export {
+  generateTemporaryCredentials,
+  createTemporaryUser,
+  deleteTemporaryUserById,
+  deleteTemporaryUserByEmail,
+};
+
+export type { Credentials };
