851db103baeacaadf11eb4aa062742e9476cbe0c
Paul <github@goonie.nl>
2025-05-29 11:19:42 +0200

    Updated Rides tests to new routing

--- tests/rides.spec.ts
+++ tests/rides.spec.ts
@@ -7,6 +7,8 @@ test.describe('Rides', () => {
     await page.getByLabel('Email Address').fill('test@goonie.nl');
     await page.getByLabel('Password').fill('test1234');
     await page.getByRole('button', { name: 'Sign In' }).click();
+
+    await page.getByText('Log a ride').click();
   });
 
   test('Renders without issues', async ({ page }) => {
@@ -19,6 +21,13 @@ test.describe('Rides', () => {
     await expect(page.getByText('Danse Macabre')).toBeVisible();
     await expect(page.getByText('Baron 1898')).toBeVisible();
     await expect(page.getByText('Joris en de Draak')).toBeVisible();
-    await expect(page.getByText('(Rood, Blauw)')).toBeVisible();
+
+    await expect(page.getByRole('button', { name: 'Log Ride' })).toHaveCount(4);
+    await expect(
+      page.getByRole('button', { name: 'Log Ride (Rood)' }),
+    ).toHaveCount(1);
+    await expect(
+      page.getByRole('button', { name: 'Log Ride (Blauw)' }),
+    ).toHaveCount(1);
   });
 });
